50aff8a0dc8779c753b28c0e1c44eaea0e577d2c
João Pedro Sousa Cavalcante <165834158+JoaoPedroCavalcante@users.noreply.github.com>
2025-11-26 10:44:02 -0300

    Refactor database connection function
    Refactor database connection to use environment variables and improve error handling.

--- database/db.go
+++ database/db.go
@@ -1,24 +1,43 @@
 package database
 
 import (
-	"log"
+    "fmt"
+    "log"
+    "os"
 
-	"github.com/guilhermeonrails/api-go-gin/models"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "github.com/guilhermeonrails/api-go-gin/models"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
-var (
-	DB  *gorm.DB
-	err error
-)
+var DB *gorm.DB
 
-func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
-	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
-	}
+func ConectaComBancoDeDados() error {
+    host := getEnv("DB_HOST", "localhost")
+    user := getEnv("DB_USER", "root")
+    password := getEnv("DB_PASSWORD", "root")
+    dbname := getEnv("DB_NAME", "root")
+    port := getEnv("DB_PORT", "5432")
 
-	DB.AutoMigrate(&models.Aluno{})
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+        host, user, password, dbname, port)
+
+    db, err := gorm.Open(postgres.Open(dsn))
+    if err != nil {
+        log.Printf("[error] failed to initialize database, got error %v", err)
+        return err
+    }
+
+    DB = db
+    if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
+        return err
+    }
+    return nil
+}
+
+func getEnv(key, defaultVal string) string {
+    if v := os.Getenv(key); v != "" {
+        return v
+    }
+    return defaultVal
 }
