5adda51e342936d882fc913a2ce9373660d62211
João Pedro Sousa Cavalcante <165834158+JoaoPedroCavalcante@users.noreply.github.com>
2025-11-26 10:47:45 -0300

    Refactor database connection setup

--- database/db.go
+++ database/db.go
@@ -1,43 +1,24 @@
 package database
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"log"
 
-    "github.com/guilhermeonrails/api-go-gin/models"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"github.com/guilhermeonrails/api-go-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	DB  *gorm.DB
+	err error
+)
 
-func ConectaComBancoDeDados() error {
-    host := getEnv("DB_HOST", "localhost")
-    user := getEnv("DB_USER", "root")
-    password := getEnv("DB_PASSWORD", "root")
-    dbname := getEnv("DB_NAME", "root")
-    port := getEnv("DB_PORT", "5432")
+func ConectaComBancoDeDados() {
+	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
 
-    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-        host, user, password, dbname, port)
-
-    db, err := gorm.Open(postgres.Open(dsn))
-    if err != nil {
-        log.Printf("[error] failed to initialize database, got error %v", err)
-        return err
-    }
-
-    DB = db
-    if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
-        return err
-    }
-    return nil
-}
-
-func getEnv(key, defaultVal string) string {
-    if v := os.Getenv(key); v != "" {
-        return v
-    }
-    return defaultVal
+	DB.AutoMigrate(&models.Aluno{})
 }
